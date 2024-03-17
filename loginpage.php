3c098a6ea1db6d057a2270d8135611898e011228
tidalcurrent <sidneysteward@gmail.com>
2024-03-17 01:28:21 -0400

    working login database

--- loginpage.php
+++ loginpage.php
@@ -5,11 +5,11 @@
 $con = mysqli_connect('localhost', 'root', '','db_login');
 
 // get the post records
-$uname = $_POST['uname'];
-$pwd = $_POST['pwd'];
+$txtuname = $_POST['txtuname'];
+$txtpwd = $_POST['txtpwd'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_login` (`Id`, `flduname`, `fldpwd`) VALUES ('0', 'uname', 'pwd')";
+$sql = "INSERT INTO `tbl_login` (`Id`, `flduname`, `fldpwd`) VALUES ('0', 'txtuname', 'txtpwd')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);

1a7698dcfdaf9888e85d069d7b32d9144864f1cb
tidalcurrent <sidneysteward@gmail.com>
2024-03-17 01:19:36 -0400

    login database fixed

--- loginpage.php
+++ loginpage.php
@@ -2,23 +2,21 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '','db_contact');
+$con = mysqli_connect('localhost', 'root', '','db_login');
 
 // get the post records
-$txtName = $_POST['txtName'];
-$txtEmail = $_POST['txtEmail'];
-$txtPhone = $_POST['txtPhone'];
-$txtMessage = $_POST['txtMessage'];
+$uname = $_POST['uname'];
+$pwd = $_POST['pwd'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+$sql = "INSERT INTO `tbl_login` (`Id`, `flduname`, `fldpwd`) VALUES ('0', 'uname', 'pwd')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
 
 if($rs)
 {
-	echo "Contact Records Inserted";
+	echo "Login Entered";
 }
 
 ?>

df3cff96c452b3c578ed704150ce1e7d0bd057ad
tidalcurrent <sidneysteward@gmail.com>
2024-03-17 20:47:20 -0400

    register added

--- registerpage.php
+++ registerpage.php
@@ -2,21 +2,21 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '','db_login');
+$con = mysqli_connect('localhost', 'root', '','db_register');
 
 // get the post records
 $txtuname = $_POST['txtuname'];
 $txtpwd = $_POST['txtpwd'];
 
 // database insert SQL code
-$sql = "INSERT INTO tbl_login (Id, flduname, fldpwd) VALUES ('0', '$txtuname', '$txtpwd')";
+$sql = "INSERT INTO tbl_register (Id, flduname, fldpwd) VALUES ('0', '$txtuname', '$txtpwd')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
 
 if($rs)
 {
-	echo "Login Entered";
+	echo "User Registered";
 }
 
 ?>

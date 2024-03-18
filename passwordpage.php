387fd16c200afbeccf303cdd1197706e40989a29
tidalcurrent <sidneysteward@gmail.com>
2024-03-18 00:08:11 -0400

    database update

--- passwordpage.php
+++ passwordpage.php
@@ -5,11 +5,10 @@
 $con = mysqli_connect('localhost', 'root', '','db_password');
 
 // get the post records
-$txtuname = $_POST['txtuname'];
 $txtpwd = $_POST['txtpwd'];
 
 // database insert SQL code
-$sql = "INSERT INTO tbl_password (Id, flduname, fldpwd) VALUES ('0', '$txtuname', '$txtpwd')";
+$sql = "INSERT INTO tbl_password (Id, fldpwd) VALUES ('0', '$txtpwd')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
---
+++ securitypage.php
@@ -0,0 +1,23 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', '','db_security');
+
+// get the post records
+$txtq1 = $_POST['txtq1'];
+$txtq2 = $_POST['txtq2'];
+$txtq2 = $_POST['txtq3'];
+
+// database insert SQL code
+$sql = "INSERT INTO tbl_security (Id, fldq1, fldq2, fldq3) VALUES ('0', '$txtq1', '$txtq2', '$txtq3')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Security Questions Updated";
+}
+
+?>

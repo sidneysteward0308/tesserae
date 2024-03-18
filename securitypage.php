7362b493435b13f677ba1c8a055796ade2955cdf
tidalcurrent <sidneysteward@gmail.com>
2024-03-18 00:19:45 -0400

    Update securitypage.php

--- securitypage.php
+++ securitypage.php
@@ -7,7 +7,7 @@ $con = mysqli_connect('localhost', 'root', '','db_security');
 // get the post records
 $txtq1 = $_POST['txtq1'];
 $txtq2 = $_POST['txtq2'];
-$txtq2 = $_POST['txtq3'];
+$txtq3 = $_POST['txtq3'];
 
 // database insert SQL code
 $sql = "INSERT INTO tbl_security (Id, fldq1, fldq2, fldq3) VALUES ('0', '$txtq1', '$txtq2', '$txtq3')";
